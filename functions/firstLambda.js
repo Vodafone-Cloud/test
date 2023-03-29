9116e5fadaf812826361ac4d0e88f163d842a419
hoeppener <127937567+hoeppener@users.noreply.github.com>
2023-03-29 22:01:07 +0200

    First commit

---
+++ functions/firstLambda.js
@@ -0,0 +1,22 @@
+'use strict';
+const fs = require('fs');
+const papa = require ('papaparse');
+
+module.exports.handler = async (event) => {
+    const stage = process.env.STAGE;
+    const file = fs.createReadStream(`books-to-list-${stage}.csv`);
+    const jsonData = await tojson(file)
+    return jsonData
+}
+
+const tojson = (file) => {
+    return new Promise((resolve, reject) => {
+        papa.parse(file, {
+            header: true, complete(results) {
+                resolve({ data: results.data })
+            }, error(err) {
+                reject(err)
+            }
+        })
+    })
+}
---
+++ functions/gfn-collector.js
@@ -0,0 +1,11 @@
+'use strict';
+const fs = require('fs');
+const papa = require ('papaparse');
+const openai = require('openai');
+
+module.exports.collector = async (event) => {
+    console.log("invoked")
+
+    console.log("This is where the magic should happen")
+    
+}
